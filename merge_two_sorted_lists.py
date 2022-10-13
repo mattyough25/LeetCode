a7acaff754fa09576fb3ae0af29b298735c101d6
mattyough25 <mgy0003@mix.wvu.edu>
2022-10-13 13:39:15 -0400

    add to problem set

--- merge_two_sorted_lists.py
+++ merge_two_sorted_lists.py
@@ -34,4 +34,58 @@ class ListNode:
         self.val = val
         self.next = next
 class Solution:
-    def mergeTwoLists(self, list1: ListNode, list2: ListNode) -> ListNode:
+    def mergeTwoLists(self, l1: ListNode, l2: ListNode) -> ListNode:
+    # Check if either of the lists is null
+        if l1 is None:
+            return l2
+        if l2 is None:
+            return l1
+        # Choose head which is smaller of the two lists
+        if l1.val < l2.val:
+            temp = head = ListNode(l1.val)
+            l1 = l1.next
+        else:
+            temp = head = ListNode(l2.val)
+            l2 = l2.next
+        # Loop until any of the list becomes null
+        while l1 is not     None and l2 is not None:
+            if l1.val < l2.val:
+                temp.next = ListNode(l1.val)
+                l1 = l1.next
+            else:
+                temp.next = ListNode(l2.val)
+                l2 = l2.next
+            temp = temp.next
+        # Add all the nodes in l1, if remaining
+        while l1 is not None:
+            temp.next = ListNode(l1.val)
+            l1 = l1.next
+            temp = temp.next
+        # Add all the nodes in l2, if remaining
+        while l2 is not None:
+            temp.next = ListNode(l2.val)
+            l2 = l2.next
+            temp = temp.next
+
+        return head
+
+# Test Cases
+s1 = Solution()
+list1 = ListNode([1,2,4])
+list2 = ListNode([1,3,4])
+
+list1.next = ListNode(5)
+print(list1.val)
+#out = s1.mergeTwoLists(list1,list2)
+#print(out.val)
+'''
+s2 = Solution()
+list1 = ListNode()
+list2 = ListNode()
+out = s2.mergeTwoLists(list1,list2)
+
+s3 = Solution()
+list1 = ListNode()
+list2 = ListNode(0)
+out = s3.mergeTwoLists(list1,list2)
+'''
